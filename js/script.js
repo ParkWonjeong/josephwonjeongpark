baf92df3a62d968ef8b9dcc3f06f5c0cd5aba001
Joseph Park <josephwjp8@gmail.com>
2025-03-22 01:58:13 -0700

    Design: added descriptions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('header nav ul li a');
     links.forEach(link => {
-      link.addEventListener('click', function(e) {
+        link.addEventListener('click', function(e) {
         e.preventDefault();
         const targetId = this.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
         if(targetSection){
-          targetSection.scrollIntoView({ behavior: 'smooth' });
+            targetSection.scrollIntoView({ behavior: 'smooth' });
         }
-      });
+        });
     });
-  });
+});
+
+document.querySelectorAll('.accordion-header').forEach(header => {
+    header.addEventListener('click', () => {
+        let content = header.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+});
   
